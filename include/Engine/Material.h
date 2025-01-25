377077e0d975eccd28b6c0de9c8522b15dfd8811
SMedd69 <s.meddahi.fo@gmail.com>
2025-01-25 08:17:59 +0100

    Ajout Code pour Material

---
+++ include/Engine/Material.h
@@ -0,0 +1,23 @@
+#ifndef __MATERIAL_H__
+#define __MATERIAL_H__
+#include <Utilities/Texture.h>
+#include <Engine/Color.h>
+
+class Material
+{
+    public:
+        Material();
+        Material(Texture* ambientTexture, Texture* diffuseTexture, Texture* specularTexture, Color ambientColor, Color diffuseColor, Color specularColor, float shininess = 32.0f);
+
+        Color m_ambientColor;
+        Color m_diffuseColor;
+        Color m_specularColor;
+
+        Texture* m_ambientTexture;
+        Texture* m_diffuseTexture;
+        Texture* m_specularTexture;
+
+        float m_shininess = 32.0f;
+};
+
+#endif
---
+++ src/Engine/Material.cpp
@@ -0,0 +1,19 @@
+#include <Engine/Material.h>
+
+Material::Material()
+{
+    m_diffuseTexture = nullptr;
+    m_specularTexture = nullptr;
+}
+Material::Material(Texture* ambientTexture, Texture* diffuseTexture, Texture* specularTexture, Color ambientColor, Color diffuseColor, Color specularColor, float shininess)
+{
+    m_ambientColor = ambientColor;
+    m_diffuseColor = diffuseColor;
+    m_specularColor = specularColor;
+
+    m_ambientTexture = ambientTexture;
+    m_diffuseTexture = diffuseTexture;
+    m_specularTexture = specularTexture;
+
+    m_shininess = shininess;
+}
